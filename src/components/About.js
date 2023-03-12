6e1162302b3fcb681745a3f37997ce5955082a86
maanasb01 <maanasb01@gmail.com>
2023-03-12 15:12:06 +0530

    Some minor Changes

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export default function About(props) {
     <div className="container border p-5" style={{color:(mode?"white":"black")}}>
       <p>
         TextHive is a powerful text utility application that helps you
-        manipulate and analyze text easily. With Textify, you can count the
+        manipulate and analyze text easily. With TextHive, you can count the
         number of words and characters in your text, calculate the reading time,
         and convert your text to uppercase or lowercase. You can also remove
         extra spaces and copy and paste text effortlessly. Plus, with the dark
@@ -16,13 +16,13 @@ export default function About(props) {
 
       <p>
         Whether you're a student, writer, or just someone who works with text
-        frequently, Textify is the perfect tool to simplify your work and
+        frequently, TextHive is the perfect tool to simplify your work and
         enhance your productivity. Try it today and experience the power of text
         manipulation at your fingertips!
       </p>
 
       <p>
-        This is a mini React project done by Maanas Bhardwaj. The referance and inspiration for this mini project is taken from a Code With Harry's YouTube Video. 
+        This is a mini React project done by Maanas Bhardwaj. 
       </p>
     </div>
   );
